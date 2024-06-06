fc3a51fb185f9d4d1438fff2865638f4f3838fb6
VladTca <votsats@gmail.com>
2024-06-06 20:28:37 +0200

    :art:

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
 
-export const VideoHeader = (props: {videoName: string}) => {
-    return <div>
-        😀 {props.videoName}
-    </div>
-}
-export const VideoContent = (props: {videoContent: string}) => {
-    return <div>
-        📼 <a href={props.videoContent}>{props.videoContent}</a>
-    </div>
-}
-export const VideoDescription = (props: {videoDescription: string}) => {
-    return <div>
-        📑 {props.videoDescription}
-    </div>
-}
-export const YoutubeVideo = (props: any) => {
-    return <div>
-        <VideoHeader videoName={props.video.title} />
-        <VideoContent videoContent={props.video.link} />
-        <VideoDescription videoDescription={props.video.description} />
-    </div>
+
+import './index.css';
+function Button() {
+    return <button
+        onClick={()=>alert('')}
+    >Click</button>
 }
 export const App = () => {
-    const video = {
-        title: 'Samurai way',
-        link: 'https://www.youtube.com/watch?v=gb7gMluAeao&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8',
-        description: 'Best free react-course'
-    }
-    return <YoutubeVideo video={video} />
-}
+<div>
+    <Button />
+</div>}
 
 export default App
